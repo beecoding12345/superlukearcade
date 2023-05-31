9f9ddf1327d81b73f98937e33bb6773765674461
beecoding12345 <94185861+beecoding12345@users.noreply.github.com>
2023-05-31 12:42:01 +0100

    fixed all materials on both levels and made a finish line for the game

--- superlukearcade/Assets/Scripts/PortalScript.cs
+++ superlukearcade/Assets/Scripts/PortalScript.cs
@@ -6,12 +6,20 @@ using UnityEngine.SceneManagement;
 public class PortalScript : MonoBehaviour
 {
 
+    
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Made a if collison tag so when the player hits the portal 1 in level 1 it loads game level 2 and comes out portal 2.
         if (collision.tag == "Player")
         {
             SceneManager.LoadScene("GameLevel2");
+        }
+
+        // Made a if collision tage so when the player runs into the finish line at the end of level 2 then it goes to game winnning screen.
+        if (collision. tag == "Player")
+        {
+            SceneManager.LoadScene("GameWinningScreen");
         }
     }
 }
