b3ed250d340daa7439497729785937e2419fd092
beecoding12345 <94185861+beecoding12345@users.noreply.github.com>
2023-06-06 09:39:33 +0100

    added some line of comments

--- superlukearcade/Assets/Scripts/PortalScript.cs
+++ superlukearcade/Assets/Scripts/PortalScript.cs
@@ -5,13 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class PortalScript : MonoBehaviour
 {
-
+    // varaible created for to load the level 
     public string levelToLoad;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //Debug.Log("Player hit the portal 1 ");
-
+        //you can load whatever level you tell it to load 
         SceneManager.LoadScene(levelToLoad);
     }
 
